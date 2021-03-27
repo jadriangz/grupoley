006f08dfb1c1ce2c65d1bb314b089cc5dff931d7
erickabrego <programador2@grupoley.com>
2021-03-27 18:46:51 +0000

    Helpdesk customer

--- helpdesk_customers/__manifest__.py
+++ helpdesk_customers/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Helpdesk',
-    'version': '0.1',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base','helpdesk','contacts'],
